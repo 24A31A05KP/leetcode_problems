42d2f1b1c3bb1702bd853d0da19448d2b90d99e6
ILLA SAI BHARAT KUMAR <illasaibharatkumar@gmail.com>
2026-09-01 19:15:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (21.50%) - LeetSync

---
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
@@ -0,0 +1,8 @@
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        target=[0]*len(nums)
+        for i in range(len(nums)):
+            for j in range(len(nums)-1,index[i],-1):
+                target[j]=target[j-1]
+            target[index[i]]=nums[i]
+        return target
