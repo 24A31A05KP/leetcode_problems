55c1937834cc0d96011c2486fc8d771af76d415c
ILLA SAI BHARAT KUMAR <illasaibharatkumar@gmail.com>
2026-09-22 07:54:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (26.76%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        arr=[]
+        nums.sort()
+        for i in range(0,len(nums)-1,2):
+            arr.extend([nums[i+1],nums[i]])
+        return arr
